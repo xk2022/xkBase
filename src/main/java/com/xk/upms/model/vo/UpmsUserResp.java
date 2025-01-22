f93483a5ce8fe4194b518d410a4eaef33362e8fe
林庚翰 <>
2025-01-22 14:49:08 +0800

    調整upmsuser req resp

--- src/main/java/com/xk/upms/model/vo/UpmsUserResp.java
+++ src/main/java/com/xk/upms/model/vo/UpmsUserResp.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import lombok.Data;
 
 @Data
-public class UpmsUserResp {
+public class UpmsUserResp  {
 
 	private String id;
 
@@ -20,5 +20,20 @@ public class UpmsUserResp {
 	private Date lastLogin;
 	
 	private Boolean locked;
+	
+	public UpmsUserResp(String id,String username,String email,String cellPhone,Date lastLogin, Boolean locked) {
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.cellPhone = cellPhone;
+		this.lastLogin = lastLogin;
+		this.locked = locked;
+	}
+	
+	public UpmsUserResp() {
+		
+	}
+
+
 
 }
